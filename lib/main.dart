fa749b19b5d40e393222796ec4b76a70dfaaba30
Axel <axelsamirm8@gmail.com>
2024-09-05 00:40:49 -0600

    Se volvio a configurar el proyecto se inicializo firebase y se modificaron las fuentes predeterminadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart'; // Importa este paquete para modificar la barra de estado
 import 'Screens/splash_screen.dart';
+import 'Screens/login_screen.dart'; // Importa la pantalla de login
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); // Asegura la inicialización de los widgets de Flutter
+  WidgetsFlutterBinding.ensureInitialized(); // Asegura la inicialización de los widgets de Flutter
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   ); // Inicializa Firebase con las opciones correctas
 
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent, // Hacer la barra de estado transparente
-    statusBarIconBrightness: Brightness
-        .dark, // Cambia el brillo del texto de la barra de estado según el fondo
-    systemNavigationBarColor: Colors
-        .transparent, // Hacer la barra de navegación inferior transparente (opcional)
+    statusBarIconBrightness: Brightness.dark, // Cambia el brillo del texto de la barra de estado según el fondo
+    systemNavigationBarColor: Colors.transparent, // Hacer la barra de navegación inferior transparente (opcional)
   ));
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -30,15 +30,18 @@ class MyApp extends StatelessWidget {
       title: 'ReportNic',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        fontFamily: 'Montserrat', // Configura la fuente globalmente
-        scaffoldBackgroundColor: Colors
-            .transparent, // Fondo del Scaffold transparente para que el fondo cubra toda la pantalla
+        fontFamily: 'Jost', // Configura la fuente globalmente
+        scaffoldBackgroundColor: Colors.transparent, // Fondo del Scaffold transparente para que el fondo cubra toda la pantalla
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.transparent, // Hacer el AppBar transparente
           elevation: 0, // Quitar la sombra del AppBar
         ),
       ),
       home: const SplashScreen(),
+      routes: {
+        '/login': (context) => const LoginScreen(), // Asegúrate de tener esta clase
+        // Agrega otras rutas aquí si es necesario
+      },
     );
   }
 }
